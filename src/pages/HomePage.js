a13019cb42a0c9cf2f0e5edf9875e95374e549ad
Egbadon Victor <thethinkerx100@gmail.com>
2022-05-10 14:49:51 +0100

    broke homepage into components

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../app.scss"
+import Directory from "../components/Directory";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory/>
+    </div>
+  );
+}
